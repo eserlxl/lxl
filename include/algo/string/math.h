de26999fee2845f9a8f71494d6712f663f3fdefc
Eser KUBALI <eserkubali@yahoo.com.tr>
2024-01-31 12:35:15 +0300

    roundStr function was implemented.

--- include/algo/string/math.h
+++ include/algo/string/math.h
@@ -9,5 +9,12 @@ namespace lxl {
     double value(const std::string &s);
 
     double _value(const std::string &s);
+
+    template<typename T> std::string roundStr(T x, uzi decimalPlaces)
+    {
+        std::string text = std::to_string(x);
+        std::vector<std::string> textArray = explode(".",text);
+        return textArray[0]+"."+textArray[1].substr(0,decimalPlaces);
+    }
 }
 #endif // lxl_ALGO_STRING_MATH_H_
