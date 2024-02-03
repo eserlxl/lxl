7bd0f30b95e8357cf13ec62c3c831954926516c7
Eser KUBALI <eserkubali@yahoo.com.tr>
2024-02-04 00:00:35 +0300

    Format change

--- include/algo/string/math.h
+++ include/algo/string/math.h
@@ -14,10 +14,9 @@ namespace lxl {
     std::string roundStr(T x, uzi decimalPlaces) {
         std::string text = std::to_string(x);
         matrixString1D textArray = explode(".", text);
-        if(decimalPlaces>0){
+        if (decimalPlaces > 0) {
             return textArray[0] + "." + textArray[1].substr(0, decimalPlaces);
-        }
-        else {
+        } else {
             return textArray[0];
         }
     }
